b1f605c2bfb3f3cc7e80213b5ab297a34e4cccae
Stas <perepelitsa@art-automation.ru>
2012-05-10 01:09:45 +0000

    funny bug is fixed

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -4,6 +4,14 @@
  *  Created on: 05.04.2012
  *      Author: perepelitsa
  */
-
+#include "Arduino.h"
 #include "Utils.h"
+
+unsigned long Utils::millis() {
+	return ::millis();
+}
+
+int Utils::ntoi(uint8_t *ref){
+	return 0;
+}
 
