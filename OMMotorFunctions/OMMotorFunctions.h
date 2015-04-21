34d4278715880adec5383ebe68d889c2b87f4903
Michael Ploof <michael@dynamicperception.com>
2015-04-21 14:06:47 -0400

    Changed lead-in / lead-out values to unsigned longs

--- OMMotorFunctions/OMMotorFunctions.h
+++ OMMotorFunctions/OMMotorFunctions.h
@@ -504,8 +504,8 @@ public:
     void checkRefresh();
     uint8_t checkStep();
 
-    void planLeadIn(unsigned int);
-	void planLeadOut(unsigned int);
+    void planLeadIn(unsigned long);
+	void planLeadOut(unsigned long);
     unsigned long planLeadIn();
 	unsigned long planLeadOut();
 
